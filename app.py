4ec72c87e021c5e41efdd99ac74e77322c80b7ca
Chris Conner <cconner@cloudera.com>
2018-06-19 11:23:36 -0400

    First test run

--- app.py
+++ app.py
@@ -2,20 +2,23 @@ from flask import Flask
 import requests
 import json
 import argparse
+import sys
 import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-o', '--oauth', action='store', dest='oauth',
                     help='SmartThings Rest API OAuth Token')
 
-parser.add_argument('-b', '--bridgehost' action='store', dest='bridgehost',
+parser.add_argument('-b', '--bridgehost', action='store', dest='bridgehost',
                     help='Hostname of the SmartThings Bridge')
 
-parser.add_argument('-p', '--bridgeport' action='store', dest='bridgeport',
+parser.add_argument('-p', '--bridgeport', action='store', dest='bridgeport',
                     help='Port of the SmartThings Bridge')
 
-parser.add_argument('-s', '--bridgessl' action="store_true", dest='bridgessl',
+parser.add_argument('-s', '--bridgessl', action="store_true", dest='bridgessl',
                     default=False, help='Enable SSL for the SmartThings Bridge')
 
 args = parser.parse_args()
@@ -56,29 +59,35 @@ def bye():
 
 @app.route("/getdevices")
 def getdevices():
-    response = requests.get(devices_uri, headers=stapi_headers)
-    json_response = json.loads(response.text)
+    devices_response = requests.get(devices_uri, headers=stapi_headers)
+    devices_json_response = json.loads(devices_response.text)
+    logging.warn("devices_uri: %s: devices_json_response:" % devices_uri)
+    logging.warn("%s" % devices_json_response[0])
 
-    for device in json_response:
+    for device in devices_json_response:
         name = device["displayName"]
         id = device["id"]
         device_uri = "%s/device/%s" % (base_uri, id)
-        response = requests.get(device_uri, headers=stapi_headers)
-        json_response = json.loads(response.text)
+        device_response = requests.get(device_uri, headers=stapi_headers)
+        device_json_response = json.loads(device_response.text)
+        logging.warn("device_uri: %s: device_json_response:" % devices_uri)
+        logging.warn("%s" % device_json_response[0])
         attrs = {"switch", "power", "level"}
-        logging.warn("supportedAttributes: %s" % json_response["supportedAttributes"])
+        logging.warn("supportedAttributes: %s" % device_json_response["supportedAttributes"])
         for attribute in attrs:
             #    if attribute in json_response["supportedAttributes"]:
             if attribute == "power":
-                attribute_uri = "%s/attribute/%s" % (device_uri, attribute)
-                response = requests.get(attribute_uri, headers=stapi_headers)
-                attr_json_response = json.loads(response.text)
+                attr_uri = "%s/attribute/%s" % (device_uri, attribute)
+                attr_response = requests.get(attr_uri, headers=stapi_headers)
+                attr_json_response = json.loads(attr_response.text)
+                logging.warn("attr_uri: %s: attr_json_response:" % attr_uri)
+                logging.warn("%s" % attr_json_response[0])
                 attr_value = attr_json_response["value"]
                 bridge_post_data = {"name": name, "value": attr_value, "type": attribute}
                 logging.warn("Pushing to bridge: name: %s: id: %s: attr: %s: value: %s" % (name, id, attribute, attr_value))
-                response = requests.post(stbridge_uri, data=json.dumps(bridge_post_data), headers=stbridge_headers)
+#                bridge_push_response = requests.post(stbridge_uri, data=json.dumps(bridge_post_data), headers=stbridge_headers)
 
-    return json_response
+    return devices_json_response
 
 
 if __name__ == '__main__':
